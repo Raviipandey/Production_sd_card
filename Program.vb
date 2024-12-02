0128b835cc2e1779375a50feac3b0231651735ec
Ravi Pandey <89774714+Raviipandey@users.noreply.github.com>
2024-12-02 16:57:44 +0530

    Final code

---
+++ Program.vb
@@ -0,0 +1,11 @@
+﻿Imports System.IO
+
+Friend Module MainProgram
+    <STAThread()>
+    Friend Sub Main(args As String())
+        Application.SetHighDpiMode(HighDpiMode.SystemAware)
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1)
+    End Sub
+End Module
